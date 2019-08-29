47801b9006a6043373ea208a0a3e3c8437379d81
parkaerin <arparkb@lsis.com>
2019-08-29 16:45:58 +0900

    test2

--- applications/config-server/src/main/java/com/example/lsis/ConfigServerApplication.java
+++ applications/config-server/src/main/java/com/example/lsis/ConfigServerApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConfigServerApplication {
     public static void main(String... args) {
 
-        SpringApplication.run(OrderServiceApplication.class, args);
+        //SpringApplication.run(OrderServiceApplication.class, args);
     }
 }
